62073dfd505ba7aa080a6e9a8a6631360376c9aa
Nick Ulle <nick.ulle@gmail.com>
2023-09-06 16:49:31 -0700

    Make train.py save metadata for linked files.

--- src/train.py
+++ src/train.py
@@ -2,23 +2,13 @@
 """
 
 from pathlib import Path
+import sys
 import tomllib
 
+import pandas as pd
+
+from . import cli
 from . import io
-
-
-def fly_dataset_subdirs(path):
-    path = Path(path)
-
-    photos_path = path / "photos"
-    labels_path = Path("staging").joinpath(*path.parts[1:]) / "labels"
-
-    print(f"{photos_path=}")
-    print(f"{labels_path=}")
-    assert photos_path.is_dir()
-    assert labels_path.is_dir()
-
-    return photos_path, labels_path
 
 
 def main():
@@ -28,44 +18,59 @@ def main():
         config = tomllib.load(f)
 
     config = config["training"]
-
-    # Check that the data sets exist.
-    dataset_dirs = [Path(p) for p in sorted(config["datasets"])]
+    data_dir = Path(config["data_dir"])
 
     # Make a directory for the training data.
     out_dir = Path(config["assembly_dir"])
+    if next(out_dir.iterdir(), None):
+        msg = ("Output directory contains files. "
+               "Continue and possibly overwrite (y/n)? ")
+        if not cli.prompt_yes(msg):
+            sys.exit(1)
 
     out_photos_dir = out_dir / "photos"
     out_photos_dir.mkdir(parents = True, exist_ok = True)
     out_labels_dir = out_dir / "labels"
     out_labels_dir.mkdir(parents = True, exist_ok = True)
 
-    print()
+    print("\nLinking files:")
 
     # Link in the images and labels.
-    count = 1
-    for dataset_path in dataset_dirs:
-        dataset = io.FlyDatasetReader(dataset_path)
-        labels_dir = Path("staging").joinpath(*dataset_path.parts[1:])
-        labels_dir /= "labels"
+    count = 0
+    crosswalk = []
+    for name in sorted(config["datasets"]):
+        dataset = io.FlyDatasetReader(data_dir / name)
 
-        for p in dataset:
+        for i, p in enumerate(dataset):
             # Link to image file.
             out_path = out_photos_dir / (f"{count:04}" + p.suffix)
+            out_path.unlink(missing_ok = True)
             out_path.hardlink_to(p)
             print(f"  '{p}' -> '{out_path}'")
 
             # Check for and link to label file.
-            label_path = labels_dir / (p.stem + ".txt")
-            assert label_path.exists()
+            label_path = dataset.label_paths[i]
+            metadata = {
+                "id": count
+                , "source_photo": p, "source_label": label_path
+                , "linked_photo": out_path
+            }
 
             out_path = out_labels_dir / (f"{count:04}" + label_path.suffix)
+            out_path.unlink(missing_ok = True)
             out_path.hardlink_to(label_path)
             print(f"  '{label_path}' -> '{out_path}'\n")
+
+            metadata["linked_label"] = out_path
+            crosswalk.append(metadata)
             count += 1
 
-    # Set up the training data by linking the images.
-    print(config)
+    print(f"Linked {count} files.")
+    # Save the crosswalk for the linked files as a CSV.
+    out_path = out_dir / "crosswalk.csv"
+    crosswalk = pd.DataFrame(crosswalk)
+    crosswalk.to_csv(out_path, index = False)
+    print(f"Wrote crosswalk '{out_path}'.")
 
 
 if __name__ == "__main__":

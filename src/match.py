9f5fc86029499a80a5b5ab5acccee3fa2c5ba75e
Nick Ulle <nick.ulle@gmail.com>
2023-10-10 20:24:33 -0700

    Remove old template matching code.

--- src/match.py
+++ src/match.py
@@ -5,26 +5,6 @@ import cv2
 import numpy as np
 
 
-def get_bounds(loc, template):
-    l, t = loc
-    h, w = template.shape[:2]
-    return t, l, t + h, l + w
-
-
-def extract_match(image, template, mode = cv2.TM_SQDIFF):
-    """Extract the coordinates of a template match.
-    """
-    dists = cv2.matchTemplate(image, template, mode)
-    _, _, min_loc, max_loc = cv2.minMaxLoc(dists)
-
-    if mode == cv2.TM_SQDIFF or mode == cv2.TM_SQDIFF_NORMED:
-        loc = min_loc
-    else:
-        loc = max_loc
-
-    return get_bounds(loc, template)
-
-
 def concatenate(matches):
     """Concatenate matches from the extract function.
 
